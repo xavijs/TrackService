310df612223ff6c4ea57eea668b2a3c9c31ad66b
Xavier Juan <xavier.juan@scmspain.com>
2016-05-18 20:00:02 +0200

    Delete unused classes

--- app/src/main/java/com/example/xavi/myapplication1/HttpPostLocationSender.java
+++ app/src/main/java/com/example/xavi/myapplication1/HttpPostLocationSender.java
@@ -9,7 +9,6 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
-
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
--- app/src/main/java/com/example/xavi/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/xavi/myapplication1/MainActivity.java
@@ -1,17 +1,10 @@
 package com.example.xavi.myapplication1;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
 
-import android.net.Uri;
-import android.os.StrictMode;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 
@@ -19,8 +12,6 @@ import com.google.android.gms.common.api.GoogleApiClient;
 public class MainActivity extends AppCompatActivity {
 
     GoogleApiClient mLocationClient;
-    TextView txtLong;
-    TextView txtLat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
 
         Intent mServiceIntent = new Intent(this, BackgroundService.class);
         this.startService(mServiceIntent);
-
-
 
     }
 }
