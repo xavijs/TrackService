508e18248660177dcdff7cdb6ea7ea8f459917f0
Xavier Juan <xavier.juan@scmspain.com>
2016-06-03 20:13:35 +0200

    remove unused view labels

---
+++ app/src/main/java/com/example/xavi/myapplication1/GeolocationModule.java
@@ -0,0 +1,7 @@
+package com.example.xavi.myapplication1;
+
+/**
+ * Created by xavi on 3/06/16.
+ */
+public class GeolocationModule {
+}
--- app/src/main/java/com/example/xavi/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/xavi/myapplication1/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
